158df264651c03aeb4eccb35584095d03128a05f
Rob van Groenewoud <git@robvangroenewoud.com>
2012-02-21 22:52:07 +0100

    Update examples

---
+++ examples/js/examples.js
@@ -0,0 +1,14 @@
+// Only used for examples page
+// ---------------------------
+
+!function ($) {
+
+  $(function(){
+
+    // make code pretty
+    window.prettyPrint && prettyPrint()
+
+  })
+
+
+}(window.jQuery)
