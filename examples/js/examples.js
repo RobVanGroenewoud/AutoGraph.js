4d921753d8c8d7e8c261527af154aa9270921432
Rob van Groenewoud <git@robvangroenewoud.com>
2012-02-23 23:50:19 +0100

    Example page now creates examples on the fly

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -1,14 +1,70 @@
 // Only used for examples page
 // ---------------------------
+/*globals prettyPrint:true */
 
 !function ($) {
 
-  $(function(){
+	$(function(){
 
-    // make code pretty
-    window.prettyPrint && prettyPrint()
+		// make code pretty
+		if(typeof window.prettyPrint !== "undefined")
+		{
+			prettyPrint();
+		}
 
-  })
+		function buildPreview() {
+			var optionString = "";
+			var preview = $('#preview');
+
+			preview.empty();
+			$('[data-option]').each(function(i){
+				var dataOption = $(this).attr('data-option');
+				var dataValue = $(this).val();
+				preview.attr(dataOption, dataValue);
+				if (dataValue.length)
+				{
+					var newline = (i === 0 || optionString.length < 1) ? '' : '\n';
+					optionString += newline+' '+dataOption+'="'+dataValue+'"';
+				}
+				else
+				{
+					preview.removeAttr(dataOption);
+				}
+			});
+			
+			preview.AutoGraph();
+			var tagName = $('#preview').get(0).tagName.toLowerCase();
+			$('#preview-code').text('<'+tagName + '\n' + optionString+'>\n<'+tagName+'>');
+			prettyPrint();
+		}
+
+		$('#preview-options button[type=submit]').on('click', function(evt){
+			evt.preventDefault();
+			buildPreview();
+		});
+
+		$('.presets a').on('click', function(evt){
+			evt.preventDefault();
+			if(!$(this).hasClass('noclear')){
+				$('#preview-options')[0].reset();
+			}
+
+			var preset = $(this);
+			var source = preset.get(0);
+			var i = 0;
+			for (i = 0; i < source.attributes.length; i++)
+			{
+				var a = source.attributes[i];
+				if(a.name.substring(0, 5)==="data-")
+				{
+					$('#preview-options [data-option='+a.name+']').val(a.value);
+				}
+			}
+			
+			buildPreview();
+		});
+
+	});
 
 
-}(window.jQuery)
+}(window.jQuery);
