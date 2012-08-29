aadf378ded59dc7f6a288bfda0c52cced6bb1977
Rob van Groenewoud <git@robvangroenewoud.com>
2012-08-30 00:00:21 +0200

    Changed plugin structure
    Allows global plugins settings, smarter caching and access to the chart
    via the element's data.

--- jquery.autograph.js
+++ jquery.autograph.js
@@ -13,132 +13,142 @@ if ( typeof Object.create !== 'function' ) {
 
 (function( $, window, document, undefined ) {
 
-	var settings = {
-				defaultType: 'column',
-				errorClassName: 'alert alert-error' // Matches Bootstrap v2.0
-			}; // default options
+	var AutoGraph = {
+		init: function( options, elem ) {
+			var self = this;
+			self.elem = elem;
+			self.$elem = $( elem );
+			self.progressBar = $('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div');
 
-	function createGraph(target){
-		var dataUrl = target.attr('data-graph-url');
-		var graphTitle = target.attr('data-graph-title');
-		var graphType = target.attr('data-graph-type');
-		var graphCategories = target.attr('data-graph-categories');
-		var graphYaxisText = target.attr('data-graph-yaxis-text');
-		var graphStacking = target.attr('data-graph-stacking');
+			self.options = $.extend( {}, $.fn.AutoGraph.options, options );
 
-		var graphOptions = {
-			chart: {
-				renderTo : $(target)[0],
-				type : graphType || settings.defaultType
-			},
-			credits: {
-					enabled : false
-			},
-			title: {
-				text: graphTitle
-			},
-			xAxis: {
-				categories: []
-			},
-			yAxis: {
+			self.run();
+		},
+
+		run: function() {
+			var self = this;
+
+			self.dataUrl = self.$elem.attr('data-graph-url');
+
+			// In case the Ajax call takes some time, show a progress bar.
+			// Don't show it if the call was short enough, to prevent flickering.
+			var progressBarTimer = setTimeout(function(){
+				self.$elem.prepend( self.progressBar );
+			}, 200);
+
+			self.fetch().done(function( results ) {
+				self.buildGraphOptions( results );
+				self.displayGraph();
+			}).error(function( jqXHR, textStatus, errorThrown ){
+				var errorMessage = $( "<div>Unable to load data from '" + self.dataUrl + "'</div>" ).addClass( self.options.errorClassName );
+				self.$elem.prepend( errorMessage );
+			}).complete(function() {
+				clearTimeout( progressBarTimer );
+				self.progressBar.hide();
+			});
+		},
+
+		fetch: function() {
+			return $.ajax({
+				url: this.dataUrl,
+				cache: false,
+				dataType : 'xml'
+			});
+		},
+
+		buildGraphOptions: function( results ){
+			var self = this;
+
+			var graphTitle = self.$elem.attr('data-graph-title');
+			var graphType = self.$elem.attr('data-graph-type');
+			var graphCategories = self.$elem.attr('data-graph-categories');
+			var graphYaxisText = self.$elem.attr('data-graph-yaxis-text');
+			var graphStacking = self.$elem.attr('data-graph-stacking');
+
+			var graphOptions = {
+				chart: {
+					renderTo : self.elem,
+					type : graphType || self.options.graphType
+				},
+				credits: {
+						enabled : false
+				},
 				title: {
-					text: graphYaxisText
-				}
-			},
-			plotOptions: {
-				series: {
-					stacking: graphStacking
-				}
-			},
-			series: []
-		};
+					text: graphTitle
+				},
+				xAxis: {
+					categories: []
+				},
+				yAxis: {
+					title: {
+						text: graphYaxisText
+					}
+				},
+				plotOptions: {
+					series: {
+						stacking: graphStacking
+					}
+				},
+				series: []
+			};
 
-		// In case the Ajax call takes some time, show a progress bar.
-		// Don't show it if the call was short enough, to prevent flickering.
-		var progressBar = $('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div');
-		var progressBarTimer = setTimeout(function(){
-			target.prepend(progressBar);
-		}, 200);
 
-		$.ajax({
-			url: dataUrl,
-			cache: false,
-			success: function(xml) {
-				var $xml = $(xml);
+			var $xml = $(results);
 
-				graphOptions.title.text = graphOptions.title.text || $xml.find('title').text();
-				graphOptions.yAxis.title.text = graphOptions.yAxis.title.text || $xml.find('yAxis text').text();
-				graphOptions.chart.type = graphOptions.chart.type || $xml.find('type').text();
+			graphOptions.title.text = graphOptions.title.text || $xml.find('title').text();
+			graphOptions.yAxis.title.text = graphOptions.yAxis.title.text || $xml.find('yAxis text').text();
+			graphOptions.chart.type = graphOptions.chart.type || $xml.find('type').text();
 
-				// push categories
-				var categories = graphCategories || $xml.find('categories').text();
-				$.each(categories.split(","), function(i, category) {
-					graphOptions.xAxis.categories.push(category);
+			// push categories
+			var categories = graphCategories || $xml.find('categories').text();
+			$.each(categories.split(","), function(i, category) {
+				graphOptions.xAxis.categories.push(category);
+			});
+			
+			var stacking = false;
+			// push series
+			$xml.find('series').each(function(i, series) {
+				
+				var hidden = $(series).find('hidden').text() === "true";
+				var stackVal = $(series).find('stack').text();
+				var stackId = stackVal ? stackVal : null;
+				stacking = stacking || stackVal.length > 0;
+				var seriesOptions = {
+					name: $(series).find('name').text(),
+					data: [],
+					visible: !hidden,
+					stack: stackId
+				};
+				
+				// push data points
+				var data = $(series).find('data').text();
+				$.each(data.split(","), function(i, point) {
+					seriesOptions.data.push(
+						parseInt(point, 10)
+					);
 				});
 				
-				var stacking = false;
-				// push series
-				$xml.find('series').each(function(i, series) {
-					
-					var hidden = $(series).find('hidden').text() === "true";
-					var stackVal = $(series).find('stack').text();
-					var stackId = stackVal ? stackVal : null;
-					stacking = stacking || stackVal.length > 0;
-					var seriesOptions = {
-						name: $(series).find('name').text(),
-						data: [],
-						visible: !hidden,
-						stack: stackId
-					};
-					
-					// push data points
-					var data = $(series).find('data').text();
-					$.each(data.split(","), function(i, point) {
-						seriesOptions.data.push(
-							parseInt(point, 10)
-						);
-					});
-					
-					// add it to the options
-					graphOptions.series.push(seriesOptions);
-				});
-
-				var chart = new Highcharts.Chart(graphOptions);
-			},
-			error: function(jqXHR, textStatus, errorThrown){
-				var errorMessage = $("<div>Unable to load data from '" + dataUrl + "'</div>").addClass(settings.errorClassName);
-				target.prepend(errorMessage);
-			},
-			complete: function(){
-				clearTimeout(progressBarTimer);
-				progressBar.hide();
-			}
-		});
-	}
-
-	var methods = {
-		init : function( options ) {
-			
-			if ( options ) {
-				$.extend( settings, options );
-			}
-
-			return this.each(function(){
-				var target = $(this);
-				createGraph(target);
+				// add it to the options
+				graphOptions.series.push(seriesOptions);
 			});
+
+			self.graphOptions = graphOptions;
+		},
+
+		displayGraph: function() {
+			var self = this;
+			self.chart = new Highcharts.Chart(self.graphOptions);
 		}
 	};
 
-	$.fn.AutoGraph = function(method) {
-		// Method calling logic
-		if ( methods[method] ) {
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.AutoGraph' );
-		}
+	$.fn.AutoGraph = function( options ){
+		return this.each(function(){
+
+			var autograph = Object.create( AutoGraph );
+			autograph.init( options, this );
+
+			$.data( this, 'AutoGraph', autograph);
+		});
 	};
 
 	$.fn.AutoGraph.options = {
